ccb2d26205a43d6230f9a53da60fd08dc90d7dc9
Javid <javid1231999@gmail.com>
2023-08-26 15:45:48 +0530

    comit

--- src/main/java/com/smart/dao/UserRepository.java
+++ src/main/java/com/smart/dao/UserRepository.java
@@ -1,9 +1,14 @@
 package com.smart.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.smart.entities.User;
 
 public interface UserRepository extends JpaRepository<User,Integer>{
-
+	
+	//we call a getUser..()passing email and we will get user which email matches
+	@Query("select u from User u where u.email=:email")
+	public User getUserByUserName(@Param("email") String email);
 }
